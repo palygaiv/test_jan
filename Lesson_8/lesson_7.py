aa1c7072abc7e6539710d544c98764e083264aa8
Igor Palyga <palygaiv@psblab.ru>
2026-01-21 17:55:48 +0300

    new

---
+++ Lesson_8/lesson_7.py
@@ -0,0 +1,23 @@
+import random
+from utils.helper import assist as assist1
+from utils.helper2 import your_easy_life as us
+from utils.helper2 import assist as assist2
+
+#print()
+#input()
+#int()
+
+print(random.random())
+print(f'Your price is {int(random.random() * 100)}')
+print(random.randint(0, 5))
+print(random.randrange(0, 10, 2))
+users = ['user1', 'user11', 'user100']
+print(random.choice(users ))
+
+assist1()
+
+while 'sssssssss' == 'ssssss':
+    if 'sssssssss' == 'sssssssss':
+        print(f'sssssssssssss {us}')
+
+assist2()
